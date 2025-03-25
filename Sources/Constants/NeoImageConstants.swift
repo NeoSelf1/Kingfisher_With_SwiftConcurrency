f6d4dd8c1a8dbdbcd500bd8df80220199cc2ff7f
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2025-03-25 18:11:16 +0900

    DiskStorage 클래스에서 자체적으로 만료기한 지정하게끔 expirationForPriority 변수 추가

--- Sources/Constants/NeoImageConstants.swift
+++ Sources/Constants/NeoImageConstants.swift
@@ -1,7 +1,8 @@
-public struct AssociatedKeys {
+public enum AssociatedKeys {
     public nonisolated(unsafe) static var downloadTask = "com.neoimage.UIImageView.DownloadTask"
 }
 
 public enum NeoImageConstants {
-    static public let expiration = StorageExpiration.days(7)
+    public static let expiration = StorageExpiration.days(7)
+    public static let expirationForPriority = StorageExpiration.days(3)
 }
